c5839665b6c9760b89a405c9c8bb302177eb4b31
adt1710 <adrianoibira.adt@gmail.com>
2022-08-30 10:43:21 -0300

    Função de adicionar um novo cadastro criada.

---
+++ js/cadastrar.js
@@ -0,0 +1,19 @@
+const btnCadastro = $(".formulario_botao");
+const btnTBody = $(".cadastros_tabela--corpo");
+
+btnCadastro.click(function (){
+    event.preventDefault();
+
+    let nome = $("#nome").val();
+    let idade = $("#idade").val();
+    let renda = $("#renda").val();
+
+    console.log(`Nome: ${nome} | Idade: ${idade} | Renda: R$${renda}`);
+
+    btnTBody.append(`<tr class="cadastros_linha">
+                        <td class="cadastros_coluna">${nome}</td>
+                        <td class="cadastros_coluna">${idade}</td>
+                        <td class="cadastros_coluna">R$${renda}</td>
+                        <td class="cadastros_coluna"><button class="cadastros_botao">X</button></td>
+                    </tr>`)
+});
